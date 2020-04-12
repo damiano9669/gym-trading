ca12a50ed5d5a281d3c30945cb5a24f9911d0dff
damiano <damiano.derin@gmail.com>
2020-04-12 10:47:20 +0200

    download data from url

--- gym_trading/envs/data_handler.py
+++ gym_trading/envs/data_handler.py
@@ -1,10 +1,13 @@
+import io
 import numpy as np
 import pandas as pd
+import requests
 
 
 def load_data(n_samples=1000):
-    data = pd.read_csv('gym_trading/envs/historical_data/BTC_USD.csv')[-n_samples:]
-    # data = pd.read_csv('historical_data/BTC_USD.csv')[-n_samples:]
+    url = 'https://blackpixel.altervista.org/btc_history/BTC_USD.csv'
+    response = requests.get(url).content
+    data = pd.read_csv(io.StringIO(response.decode('utf-8')))[-n_samples:]
 
     dates = data['Date']
     high = data['High']
